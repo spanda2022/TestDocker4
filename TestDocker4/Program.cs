fd06abe616f07bbcd679f7742b2aeb4765410e42
spanda2022 <Spanda@SILICA.NET>
2022-02-28 16:21:46 +0200

    testdocker4

---
+++ TestDocker4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDocker4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello This is my first windows docker container");
+        }
+    }
+}
